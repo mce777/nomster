fe680b22123e5d0a5f51b8f6876826dd507a51af
mce777 <mce777@gmail.com>
2015-09-23 19:19:16 +0000

    heroku fix2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Nomster::Application.configure do
   }
   ActionMailer::Base.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { :host => 'nomster-mark-eberhard.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'nomster-mark-eberhard.herokuapp.com' }
 
   # Settings specified here will take precedence over those in config/application.rb.
 

b04e29ce396d4602d4280a70c5ac9985afeef569
Mark Eberhard <mce777@gmail.com>
2016-01-27 18:58:47 +0000

    model change

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,9 +5,8 @@ class Place < ActiveRecord::Base
 
 	  validates :name, :presence => true, :length => { :minimum => 3 }
 	  validates :description, :address, :presence => true
-	  
-	  # no brackets or parenthesis needed on above line (fucks shit up)
-	  # check out 'sexy validation' in lesson 28 maybe
+
+	  # no brackets or parenthesis needed on above line ^
 
 	  has_many :comments #, :dependent => :destroy
 	  has_many :photos
